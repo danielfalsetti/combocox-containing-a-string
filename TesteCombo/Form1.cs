7565a7e40d54a6f340604d89e1a2ce89867b5de6
Daniel Falsetti Gahardoni <daniel.falsetti@gmail.com>
2020-09-22 17:26:04 -0300

    Tipo do campo value para long

--- TesteCombo/Form1.cs
+++ TesteCombo/Form1.cs
@@ -21,7 +21,7 @@ namespace TesteCombo
         public class ComboboxItem
         {
             public string Text { get; set; }
-            public object Value { get; set; }
+            public long Value { get; set; }
 
             public override string ToString()
             {
@@ -31,34 +31,37 @@ namespace TesteCombo
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            List<ComboboxItem> lista = new List<ComboboxItem>();
             ComboboxItem item = new ComboboxItem();
             item.Text = "Item text1";
             item.Value = 12;
 
-            this.cbCombo.Items.Add(item);
+            lista.Add(item);
 
             item = new ComboboxItem();
-            item.Text = "Item Falsetti Dániel";
+            item.Text = "Item Falsetti Daniel";
             item.Value = 32;
 
-            this.cbCombo.Items.Add(item);
+            lista.Add(item);
 
             item = new ComboboxItem();
             item.Text = "Item Dávi Dániel";
             item.Value = 33;
 
-            this.cbCombo.Items.Add(item);
+            lista.Add(item);
 
             item = new ComboboxItem();
             item.Text = "Golias Davi Dániel";
             item.Value = 35;
 
-            this.cbCombo.Items.Add(item);
+            this.cbCombo.Items.AddRange(lista.ToArray());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string teste = this.cbCombo.Text;
+            //var r = this.cbCombo.Items.Cast<ComboboxItem>().Where(w => w.Value == 33).FirstOrDefault();
+            //this.cbCombo.SelectedItem = r;
+            //string teste = this.cbCombo.Text;
             ComboboxItem t = (ComboboxItem)this.cbCombo.SelectedItem;
         }
     }
